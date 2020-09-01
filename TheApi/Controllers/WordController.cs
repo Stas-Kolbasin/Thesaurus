7bafd956d7a618152ed8ecfc52393f49bd2b821a
Stas Kolbasin <stas.kolbasin@namecheap.com>
2020-09-01 10:53:52 +0300

    Added basic API methods.

---
+++ TheApi/Controllers/WordController.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TheServices.Models;
+using TheServices.Services;
+
+namespace TheApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WordController : ControllerBase
+    {
+        private readonly IWordService _wordService;
+
+        public WordController(IWordService wordService) =>
+            _wordService = wordService;
+        
+        [HttpGet]
+        public async Task<Word[]> GetAll()
+        {
+            return await _wordService.GetAll();
+        }
+
+        [HttpGet]
+        [Route("{base}")]
+        public async Task<Word> Get(string @base)
+        {
+            return await _wordService.Get(@base);
+        }
+
+        [HttpPost]
+        public async Task Upsert(Word word)
+        {
+            await _wordService.Create(word);
+        }
+    }
+}
--- TheApi/Startup.cs
+++ TheApi/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using TheData;
+using TheServices.Services;
 
 namespace TheApi
 {
@@ -25,6 +27,8 @@ namespace TheApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IWordRepository, SqliteWordRepository>();
+            services.AddTransient<IWordService, WordService>();
             services.AddControllers();
         }
 
